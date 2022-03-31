b6d1a0e691356d348b4a3885568cc72ca5bad670
DerekChanXD <dxrek.chan@gmail.com>
2022-03-30 20:09:12 -0700

    Co-authored-by: Pythonic Otaku <t-shah02@users.noreply.github.com> Co-authored-by: claireshou <claireshou@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-print("hello world")
-for i in range(5):
-    print("tanish is not gaY")
+import cmpt120image
+
+colors_data = open("colours.csv")
+lines = colors_data.read().splitlines()
+color_dict = {}
+
+for line in lines:
+    line_split = line.split(",")
+    name = line_split[0]
+    r,g,b = line_split[1:]
+    color_dict[(r,g,b)] = name 
+
+print(color_dict)
+
+running = True
+
+while running:
+    print("\n1. Load Colour File\n2. Print All Colours\n3. Select Colour\n4. Find Closest Colour\n5. Display (Save) Colour Scheme\n0. Quit")
+    option = int(input("\nSelect the option: "))
+    
+    if option == 0:
+        running = False
+        print("Quitting the program!")
+    if option == 1:
+
+        print("The file has been processed and 10 colours were entered into the dictionary")
+    if option == 2:
+        pass
+    if option == 3:
+        pass
+    if option == 4:
+        pass
+    if option == 5:
+        pass
+
+    
+
+
+
